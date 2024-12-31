3b20a0719f29a534cd72eedd449288f2f2604330
Kowit An-chean <kowitanchean238@gmail.com>
2024-12-31 12:29:08 +0700

    button command

--- learn gui/gui.py
+++ learn gui/gui.py
@@ -17,10 +17,16 @@ myLabel = Label(text="Hello",bg="yellow").place(x=0,y=0)
 #จัดตำแหน่ง Label โดยใช้ grid ps. grid ใช้ร่วมกับวิธีอื่นไม่ได้
 # myLabel = Label(text="Hello",font=20).grid(row=0,column=0)
 
+def showmsg():
+    Label(text="KK!\n").pack()
 #button
-btn1 = Button(root,text="Open",bg="brown",font=30).pack()
+btn1 = Button(root,text="Open",fg="white",bg="black",font=30,command=showmsg).pack()
 
-#ขนาดหน้าจอ
+
+def showmsg():
+    print("KK!\n")
+
+#ขนาดหน้าจอ&ตำแหน่ง
 root.geometry("500x400+700+250")
 
 root.mainloop()
