60e341397696e9b0b769d11fffefa125085d2475
Kowit An-chean <kowitanchean238@gmail.com>
2024-12-31 11:58:17 +0700

    LABEL
    เรียนรู้วิธีใช้ Label

--- learn gui/gui.py
+++ learn gui/gui.py
@@ -3,6 +3,16 @@ from tkinter import *
 root = Tk()
 root.title("GUI")
 
+#label
+myLabel = Label(text="Hello").pack()
+#fg = color font
+myLabel = Label(text="Hello",fg="red").pack()
+#font = font size
+myLabel = Label(text="Hello",font=20).pack()
+#bg = background color
+myLabel = Label(text="Hello",bg="yellow").pack()
+
 #ขนาดหน้าจอ
 root.geometry("500x400+700+250")
+
 root.mainloop()
