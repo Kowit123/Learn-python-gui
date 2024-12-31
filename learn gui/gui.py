009d79e3644a748d7d1e5e6bc278f343dd4bfe33
Kowit An-chean <kowitanchean238@gmail.com>
2024-12-31 13:05:01 +0700

    Update gui.py

--- learn gui/gui.py
+++ learn gui/gui.py
@@ -3,28 +3,24 @@ from tkinter import *
 root = Tk()
 root.title("GUI")
 
-#label
-myLabel = Label(text="Hello").pack()
-#fg = color font
-myLabel = Label(text="Hello",fg="red").pack()
-#font = font size
-myLabel = Label(text="Hello",font=20).pack()
-#bg = background color
-myLabel = Label(text="Hello",bg="yellow").pack()
+#label, fg = color font, font = font size, bg = background color
+myLabel = Label(root,text="Hello",fg="red",font=20,bg="yellow").pack()
 #จัดตำแหน่ง Label โดยใช้ place
-myLabel = Label(text="Hello",bg="yellow").place(x=0,y=0)
-
+# myLabel = Label(root,text="Hello",bg="yellow").place(x=0,y=0)
 #จัดตำแหน่ง Label โดยใช้ grid ps. grid ใช้ร่วมกับวิธีอื่นไม่ได้
 # myLabel = Label(text="Hello",font=20).grid(row=0,column=0)
 
 def showmsg():
+    msg = txt.get()
+    Label(text=msg,bg="black",fg="white").pack()
     Label(text="KK!\n").pack()
 #button
 btn1 = Button(root,text="Open",fg="white",bg="black",font=30,command=showmsg).pack()
 
+#textbox
+txt = StringVar()
+mytxt = Entry(root,textvariable=txt).pack()
 
-def showmsg():
-    print("KK!\n")
 
 #ขนาดหน้าจอ&ตำแหน่ง
 root.geometry("500x400+700+250")
