af1ad9828aec603342784f93fe854472ce6b2572
Kowit An-chean <kowitanchean238@gmail.com>
2024-12-31 12:16:00 +0700

    button

--- learn gui/gui.py
+++ learn gui/gui.py
@@ -11,6 +11,14 @@ myLabel = Label(text="Hello",fg="red").pack()
 myLabel = Label(text="Hello",font=20).pack()
 #bg = background color
 myLabel = Label(text="Hello",bg="yellow").pack()
+#จัดตำแหน่ง Label โดยใช้ place
+myLabel = Label(text="Hello",bg="yellow").place(x=0,y=0)
+
+#จัดตำแหน่ง Label โดยใช้ grid ps. grid ใช้ร่วมกับวิธีอื่นไม่ได้
+# myLabel = Label(text="Hello",font=20).grid(row=0,column=0)
+
+#button
+btn1 = Button(root,text="Open",bg="brown",font=30).pack()
 
 #ขนาดหน้าจอ
 root.geometry("500x400+700+250")
